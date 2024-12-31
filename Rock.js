2016606924646900a9acc4abfe4a0ad212097b86
sampath <sampaths4418@gmail.com>
2024-12-31 20:14:42 +0530

    successfully modifed the project

--- Rock.js
+++ Rock.js
@@ -1,24 +1,8 @@
-console.log("hello");
+
+let flag=0;
 let humanscore=0;
 let computerscore=0;
 let n;
-
-
-function getHumanChoise(n)
-{
-    if(n<1)
-    {
-        return "Rock";
-    }
-    else if(n>=1&&n<5)
-    {
-        return "Paper";
-    }
-    else
-    {
-        return "Scissor";
-    }
-}
 function getComputerChoice()
 {
     let x=Math.random();
@@ -34,48 +18,49 @@ function getComputerChoice()
         return "Scissor";
     }
 }
+function results()
+{
+    if(humanscore==5)
+    {
+        document.getElementById("result").textContent=("congratus u won the game");
+    }
+    else if(computerscore==5)
+    {
+        document.getElementById("result").textContent=("u lost the game");
+    }
+}
 function playround(humanSelection,computerSelection)
 {
+    
     if((computerSelection=="Rock" && humanSelection=="Paper")||(computerSelection=="Paper" && humanSelection=="Scissor")||(computerSelection="Scissor"&& humanSelection=="Rock" ))
     {
         humanscore=humanscore+1;
         console.log("You won");
+        results();
     }
     else if((computerSelection=="Paper" && humanSelection=="Rock")||(computerSelection=="Scissor" && humanSelection=="Paper")||(computerSelection="Rock"&&humanSelection=="Scissor" ))
     {
         computerscore=computerscore+1;
         console.log("you lost");
+        results();
     }
     else
     {
         humanscore=humanscore;
         computerscore=computerscore;
-    }
-}
-function playgame()
-{
-    for(let i=0;i<5;i++)
-    {
-        n=parseInt(prompt("enter less than 0.5 for Rock less than .75 to paper and less than 1 and greater than .75 for scissor"));
-        let humanSelection=getHumanChoise(n);
-        let computerSelection=getComputerChoice();
-        playround(humanSelection,computerSelection);
-        console.log("the human score is",humanscore);
-        console.log("computer score",computerscore);
-    }
-    if(humanscore<computerscore)
-    {
-        console.log("you lost the game");
-    }
-    else if(humanscore>computerscore)
-    {
-        console.log("you won the game");
-    }
-    else
-    {
-        console.log("the game ended by draw");
+        results();
     }
 }
 
 
-playgame();
+
+document.getElementById("Rock").addEventListener("click",function(){
+    playround("Rock",getComputerChoice())
+});
+document.getElementById("Scissor").addEventListener("click",function(){
+    playround("Scissor",getComputerChoice())
+});
+document.getElementById("Paper").addEventListener("click",function(){
+    playround("Paper",getComputerChoice())
+});
+
